3871164775cddddcac66ef9ef9b07bfd965d85c0
TheCloneZone <49557272+TheCloneZone@users.noreply.github.com>
2019-04-12 21:24:18 +0800

    German

--- loc/factions/vanguard.js
+++ loc/factions/vanguard.js
@@ -3,12 +3,14 @@ export default {
     en: 'Vanguard',
     se: 'Vanguard',
     ru: 'Vanguard',
-    es: 'Vanguard'
+    es: 'Vanguard',
+    de: 'Vanguard'
   },
   description: {
     en: 'The Vanguard horde seeks tradition, structure and order in society. Their Castle can be found in a lush green area around Guardstone.',
     se: 'Vanguard söker tradition, struktur och ordning i samhället. Deras slott kan hittas i ett vackert grönt område omkring Guardstone.',
     ru: 'Орда Vanguard соблюдает традиции, структуру и порядок в обществе. Их крепость может быть найдена в пышной зеленой зоне около Guardstone.',
-    es: 'Los valores del Vanguard son la tradición, la estructura y el orden de sociedad. Su Castillo se halla en el bosque exuberante de Guardstone.'
+    es: 'Los valores del Vanguard son la tradición, la estructura y el orden de sociedad. Su Castillo se halla en el bosque exuberante de Guardstone.',
+    de: 'Die Vanguard Horde sucht Tradition, Struktur und Ordnung in der Gesellschaft. Ihr Schloss befindet sich in einer grünen Umgebung um Guardstone.',
   }
 }
