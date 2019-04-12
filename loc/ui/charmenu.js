767deb85376e81d0dcb1fb135d6798984f64605e
TheCloneZone <49557272+TheCloneZone@users.noreply.github.com>
2019-04-12 19:35:30 +0800

    German
    Added German (might have to be double checked by dek)

--- loc/ui/charmenu.js
+++ loc/ui/charmenu.js
@@ -6,27 +6,32 @@ export default {
     title: {
       en: 'Select Character',
       se: 'Välj Karaktär',
-      ru: 'Выберите Персонажа'
+      ru: 'Выберите Персонажа',
+      de: 'Charakter Auswählen'
     },
     create: {
       en: 'Click to create character',
       se: 'Klicka för att skapa karaktär',
-      ru: 'Нажмите, чтобы создать персонажа'
+      ru: 'Нажмите, чтобы создать персонажа',
+      de: 'Klick zum Charakter erstellen'
     },
     enterWorld: {
       en: 'Enter World',
       se: 'Starta Värld',
-      ru: 'Войти в мир'
+      ru: 'Войти в мир',
+      de: 'Welt Eintreten'
     },
     emptySlot: {
       en: 'Empty',
       se: 'Ingen',
-      ru: 'Пусто'
+      ru: 'Пусто',
+      de: 'Leer'
     },
     error: {
       en: 'Network error',
       se: 'Nätverks problem',
-      ru: 'Проблема с сетью'
+      ru: 'Проблема с сетью',
+      de: 'Netzwerkfehler'
     }
   },
 
@@ -35,32 +40,38 @@ export default {
     title: {
       en: 'Character creation',
       se: 'Skapa karaktär',
-      ru: 'Создание персонажа'
+      ru: 'Создание персонажа',
+      de: 'Charakter erstellung'
     },
     pressIcon: {
       en: 'Press an icon for a brief description.',
       se: 'Klicka en ikon för beskrivning.',
-      ru: 'Нажмите на иконку, чтобы увидеть краткое описание.'
+      ru: 'Нажмите на иконку, чтобы увидеть краткое описание.',
+      de: 'Drücken Sie ein Symbol für eine kurze Beschreibung'
     },
     selectClass: {
       en: 'Select a class',
       se: 'Välj en klass',
-      ru: 'Выберите класс'
+      ru: 'Выберите класс',
+      de: 'Wählen Sie eine Klasse'
     },
     selectFaction: {
       en: 'Select a faction',
       se: 'Välj en sida',
-      ru: 'Выберите фракцию'
+      ru: 'Выберите фракцию',
+      de: 'Wählen Sie eine Fraktion'
     },
     enterName: {
       en: 'Enter a name',
       se: 'Välj ett namn',
-      ru: 'Введите имя'
+      ru: 'Введите имя',
+      de: 'Name eingeben'
     },
     nameReq: {
       en: 'Your name can only contain characters a-Z, and numbers 0-9, without any spaces. Up to 16 characters.',
       se: 'Ditt namn kan bara använda sig karaktärerna a-Z, och nummer 0-9, utan mellanslag. Max 16 karaktärer.',
-      ru: 'Ваше имя может состоять только из латинского алфавита a-Z и цифр 0-9, без пробелов. До 16 символов.'
+      ru: 'Ваше имя может состоять только из латинского алфавита a-Z и цифр 0-9, без пробелов. До 16 символов.',
+      de: 'Dein Name darf nur Charakters von a-Z, und Numeren von 0-9, ohne leertasten enthalten. Bis zu 16 Zeichen'
     }
   },
 
@@ -70,17 +81,20 @@ export default {
     title: {
       en: 'Delete',
       se: 'Ta bort',
-      ru: 'Удалить'
+      ru: 'Удалить',
+      de: 'Löscht'
     },
     info: {
       en: 'To delete, type the characters name. Your character can not be restored.',
       se: 'För att ta bort, skriv in karaktärens namn. Din karaktär tas bort för alltid.',
-      ru: 'Чтобы удалить, введите имя персонажа. Ваш персонаж не может быть восстановлен.'
+      ru: 'Чтобы удалить, введите имя персонажа. Ваш персонаж не может быть восстановлен.',
+      de: 'Zum Löschen, geben Sie denn Charakter Name ein. Dein Charakter kann nicht wiederhergestellt werden.'
     },
     placeholder: {
       en: 'Are you sure?',
       se: 'Är du säker?',
-      ru: 'Вы уверены?'
+      ru: 'Вы уверены?',
+      de: 'Sind Sie sicher?'
     }
   }
 
