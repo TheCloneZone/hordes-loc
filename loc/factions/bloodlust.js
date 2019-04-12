72132d764b57b3e7c3ecf1e02f9061dd7a28f209
TheCloneZone <49557272+TheCloneZone@users.noreply.github.com>
2019-04-12 21:21:31 +0800

    German

--- loc/factions/bloodlust.js
+++ loc/factions/bloodlust.js
@@ -3,12 +3,14 @@ export default {
     en: 'Bloodlust',
     se: 'Bloodlust',
     ru: 'Bloodlust',
-    es: 'Bloodlust'
+    es: 'Bloodlust',
+    de: 'Bloodlust'
   },
   description: {
     en: 'The Bloodlust horde values freedom and loyalty, offers a place for individualism or even chaos. Their Defenses are located in a desert realm called Headless Landing.',
     se: 'Bloodlust värderar frihet och lojalitet, erbjunder en plats för individualism men också kaos. Deras försvar hittas i ett öken kungarike vid namn Headless Landing.',
     ru: 'Орда Bloodlust славится свободой и лояльностью, предлагая место для индивидуализма или даже хаоса. Их оборона находится в пустынном мире, названном Headless Landing.',
-    es: 'A la horda Bloodlust le importan la libertad y lealtad. Con su guarida situado en un desierto llamado Headless Landing, ofrecen el lugar perfecto para expresión y caos.'
+    es: 'A la horda Bloodlust le importan la libertad y lealtad. Con su guarida situado en un desierto llamado Headless Landing, ofrecen el lugar perfecto para expresión y caos.',
+    de: 'Die Bloodlust Horde legt Wert auf Freiheit und Loyalität, bietet Platz für Individualismus oder sogar für Chaos. Ihre Verteidigung befindet sich in einem Wüstenreich namens Headless Landing.'
   }
 }
